7f65622825e5b6ea587f70c7f75f8349902f7320
Kamina <105399662+kamina7@users.noreply.github.com>
2022-11-16 19:37:45 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,27 +11,9 @@ if [ ${INPUT_REMOTE_HOST#"ssh://"} != "$INPUT_REMOTE_HOST" ]; then
     SSH_HOST=${INPUT_REMOTE_HOST#"ssh://"}
     SSH_HOST=${SSH_HOST#*@}
 
-    if [ -z "$INPUT_SSH_PRIVATE_KEY" ]; then
-        echo "Input ssh_private_key is required for SSH hosts!"
-        exit 1
-    fi
-
-    if [ -z "$INPUT_SSH_PUBLIC_KEY" ]; then
-        echo "Input ssh_public_key is required for SSH hosts!"
-        exit 1
-    fi
-
-    echo "Registering SSH keys..."
-
-    # Save private key to a file and register it with the agent.
-    mkdir -p "$HOME/.ssh"
-    printf '%s' "$INPUT_SSH_PRIVATE_KEY" > "$HOME/.ssh/docker"
-    chmod 600 "$HOME/.ssh/docker"
-    eval $(ssh-agent)
-    ssh-add "$HOME/.ssh/docker"
-
-    # Add public key to known hosts.
-    printf '%s %s\n' "$SSH_HOST" "$INPUT_SSH_PUBLIC_KEY" >> /etc/ssh/ssh_known_hosts
+    $(echo $INPUT_SSH_KEY) > ~/.ssh/host_key
+    ssh-copy-id -i ~/.ssh/host_key ${SSH_HOST}
+    
 fi
 
 echo "Connecting to $INPUT_REMOTE_HOST..."
